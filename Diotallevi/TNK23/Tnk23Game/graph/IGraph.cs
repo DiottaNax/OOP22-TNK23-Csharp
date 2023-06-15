65ba0ea5c938526e64d8c13ef94659b08685d0f3
Nax <federico.diotallevi@studio.unibo.it>
2023-06-15 18:15:38 +0200

    added Graph interface

---
+++ Diotallevi/TNK23/Tnk23Game/graph/IGraph.cs
@@ -0,0 +1,32 @@
+
+namespace Tnk23Game.Graph
+{
+    public interface IGraph<N>
+    {
+        /// <summary>
+        /// Retrieves a set of all nodes in the graph.
+        /// </summary>
+        /// <returns>A set of nodes.</returns>
+        HashSet<N> GetNodes();
+
+        /// <summary>
+        /// Retrieves a set of adjacent nodes to the specified node.
+        /// </summary>
+        /// <param name="node">The node to get the adjacencies for.</param>
+        /// <returns>A set of adjacent nodes.</returns>
+        HashSet<N> GetAdjacencies(N node);
+
+        /// <summary>
+        /// Adds a node to the graph.
+        /// </summary>
+        /// <param name="node">The node to add.</param>
+        /// <returns>The added node.</returns>
+        N AddNode(N node);
+
+        /// <summary>
+        /// Removes a node from the graph.
+        /// </summary>
+        /// <param name="node">The node to remove.</param>
+        void RemoveNode(N node);
+    }
+}

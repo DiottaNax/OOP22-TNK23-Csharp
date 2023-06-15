e19d54883feca4742d6389603363dd665df8096a
Nax <federico.diotallevi@studio.unibo.it>
2023-06-15 20:23:02 +0200

    added WorldEventHandler

---
+++ Diotallevi/TNK23/Tnk23Game/extra/WorldEventHandler.cs
@@ -0,0 +1,14 @@
+namespace Tnk23Game.Extra
+{
+    public class WorldEventHandler : IWorldEventHandler
+    {
+        // Empty class made to avoid errors in GameLoop
+        public WorldEventHandler()
+        {
+        }
+
+        public void Handle(IWorldEvent e)
+        {
+        }
+    }
+}

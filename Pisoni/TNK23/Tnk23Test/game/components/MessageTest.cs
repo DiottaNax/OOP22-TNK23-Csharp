576aa3f4c123686f7c9f7522f975a1fb30bf38ac
Giovanni Pisoni <giovanni.pisoni@studio.unibo.it>
2023-06-16 10:30:35 +0200

    [DEV] Added 'Pisoni' package and Messagetest.cs

---
+++ Pisoni/TNK23/Tnk23Test/game/components/MessageTest.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace it.unibo.tnk23.game.components
+{
+
+    /// <summary>
+    /// The MessageTest class is responsible for testing the functionality of the Message interface.
+    /// </summary>
+    [TestClass]
+    public class MessageTest
+    {
+        /// <summary>
+        /// Test case for the getMessage() method.
+        /// It verifies that the method returns the correct message.
+        /// </summary>
+        [TestMethod]
+        public void TestGetMessage()
+        {
+            Message message = new Message()
+            {
+            GetMessage = () => "Hello World!"
+            };
+            Assert.AreEqual("Hello World!", message.GetMessage());
+        }
+    }
+}

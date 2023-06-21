eb75df04ffeb6331483225fe50588be79cea7111
Javid Ameri <javid.ameri@studio.unibo.it>
2023-06-22 01:20:48 +0200

    added Directions

---
+++ Ameri/TNK23/Tnk23Game/common/Directions.cs
@@ -0,0 +1,34 @@
+namespace Tnk23Game.common
+{
+    /// <summary>
+    /// The Directions enum represents different directions in a two-dimensional space.
+    /// Each direction is associated with a velocity vector represented by the Vector2D class.
+    /// </summary>
+    public enum Directions
+    {
+        /// <summary>
+        /// The north direction (up).
+        /// </summary>
+        NORTH,
+
+        /// <summary>
+        /// The south direction (down).
+        /// </summary>
+        SOUTH,
+
+        /// <summary>
+        /// The west direction (left).
+        /// </summary>
+        WEST,
+
+        /// <summary>
+        /// The east direction (right).
+        /// </summary>
+        EAST,
+
+        /// <summary>
+        /// No direction (stationary).
+        /// </summary>
+        NONE
+    }
+}

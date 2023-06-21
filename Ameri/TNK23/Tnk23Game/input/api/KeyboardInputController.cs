89711796537a0f8526b344c6821a87e566a34555
Javid Ameri <javid.ameri@studio.unibo.it>
2023-06-22 01:21:30 +0200

    added KeyboardInputController interface

---
+++ Ameri/TNK23/Tnk23Game/input/api/KeyboardInputController.cs
@@ -0,0 +1,30 @@
+using Tnk23Game.Input;
+
+namespace Tnk23Game.Input.Api
+{
+    /// <summary>
+    /// The KeyboardInputController interface extends the InputController interface
+    /// and represents a keyboard input controller that provides directional input
+    /// and shooting functionality.
+    /// </summary>
+    public interface KeyboardInputController : InputController
+    {
+        /// <summary>
+        /// Checks if the game object is shooting.
+        /// </summary>
+        /// <returns>true if shooting action is triggered, false otherwise</returns>
+        bool IsShooting();
+
+        /// <summary>
+        /// Sets the event handler for a key press event.
+        /// </summary>
+        /// <param name="e">The key press event handler</param>
+        void SetOnKeyPressed(int e);
+
+        /// <summary>
+        /// Sets the event handler for a key release event.
+        /// </summary>
+        /// <param name="e">The key release event handler</param>
+        void SetOnKeyReleased(int e);
+    }
+}

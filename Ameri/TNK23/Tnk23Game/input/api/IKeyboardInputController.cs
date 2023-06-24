c31780881dd1ebb0e60b064a8533d2275611df64
Javid Ameri <javid.ameri@studio.unibo.it>
2023-06-24 17:21:46 +0200

    added 2 comments

--- Ameri/TNK23/Tnk23Game/input/api/IKeyboardInputController.cs
+++ Ameri/TNK23/Tnk23Game/input/api/IKeyboardInputController.cs
@@ -13,12 +13,14 @@ namespace Tnk23Game.Input.Api
         /// <returns>true if shooting action is triggered, false otherwise</returns>
         bool IsShooting();
 
+        /// since the absence of a corrispondent C# type for javafx KeyEvent, this one has been changed into an int-type to avoid complications
         /// <summary>
         /// Sets the event handler for a key press event.
         /// </summary>
         /// <param name="e">The key press event handler</param>
         void SetOnKeyPressed(int e);
 
+        /// since the absence of a corrispondent C# type for javafx KeyEvent, this one has been changed into an int-type to avoid complications
         /// <summary>
         /// Sets the event handler for a key release event.
         /// </summary>
